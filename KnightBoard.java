b6a552ff4a487a8753b8e36108787f85eff395bb
Oliver Ripps <oripps@stuy.edu>
2019-02-13 10:29:39 -0500

    wrote toString

--- KnightBoard.java
+++ KnightBoard.java
@@ -7,7 +7,30 @@ public class KnightBoard{
     moves=b;
   }
   //Initialize the board to the correct size and make them all 0's
-  public String toString()
+  public String toString() throws IllegalStateException(){
+    String str="";
+    for(int i=0;i<board.length;i++){
+      for(int l=0;l<board[0].length;l++){//for each piece
+        if(board[i][l]>=10){//if queen
+          str+=board[i][l];
+        }
+        if(board[i][l]>0 && board[i][l]<10)
+          str+=" ";
+          str+=board[i][l];
+        }
+        if(l<board[0].length-1){
+          str+=" ";
+        }
+        if(l>=board[0].length-1){
+          str+="\n";
+        }
+
+      }
+    }
+    return str;
+  }
+  }
+
   /*see format for toString below
   blank boards display 0's as underscores
   you get a blank board if you never called solve or
