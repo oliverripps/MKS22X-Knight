942f515a96c32625139fd1c042cd20af241ca155
Oliver Ripps <oripps@stuy.edu>
2019-02-26 11:33:48 -0500

    sort is working, I guess my optimization is just not great

--- Driver.java
+++ Driver.java
@@ -1,7 +1,7 @@
 public class Driver{
 
   public static void main(String[] args){
-    KnightBoard test = new KnightBoard(6,6);
+    KnightBoard test = new KnightBoard(5,5);
     //test.solve(0,0);
     //System.out.println(test.toString());
     System.out.println(test.countSolutions(0,0));
